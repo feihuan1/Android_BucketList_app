294c7a850a73814b28122361bd08a0667794b35e
Feihuan Peng <feihuan.peng@spreetail.com>
2025-09-24 00:16:32 -0500

    asd

--- app/src/main/java/com/example/bucketlist/PlacesToGoActivity.java
+++ app/src/main/java/com/example/bucketlist/PlacesToGoActivity.java
@@ -3,6 +3,7 @@ package com.example.bucketlist;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class PlacesToGoActivity extends AppCompatActivity {
 
@@ -13,6 +14,46 @@ public class PlacesToGoActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+        setuplist();
+
+    }
+
+    private void setuplist() {
+        BucketListEntry[] PlacesToGo = {
+                new BucketListEntry(
+                        "Safari in Africa",
+                        "Experience wildlife up close in the Serengeti or Kruger National Park.",
+                        R.drawable.kerala,
+                        3.7f
+                ),
+                new BucketListEntry(
+                        "Learn to Surf",
+                        "Ride the waves in Hawaii or Australia and master surfing.",
+                        R.drawable.japan,
+                        2.5f
+                ),
+                new BucketListEntry(
+                        "Hot Air Balloon Ride",
+                        "Soar above the landscapes in Cappadocia or Napa Valley.",
+                        R.drawable.iceland,
+                        4.3f
+                ),
+                new BucketListEntry(
+                        "Explore the Amazon Rainforest",
+                        "Immerse yourself in the world's largest tropical rainforest.",
+                        R.drawable.vietnam,
+                        5.0f
+                ),
+                new BucketListEntry(
+                        "Visit the Pyramids of Giza",
+                        "Marvel at the ancient Egyptian pyramids and their history.",
+                        R.drawable.northern_lights,
+                        8.5f
+                )
+        };
+        RecyclerView recyclerView = findViewById(R.id.recycler_view_places_to_go);
+        BucketListEntryAdapter adapter = new BucketListEntryAdapter(PlacesToGo);
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
--- app/src/main/java/com/example/bucketlist/ThingsToDoActivity.java
+++ app/src/main/java/com/example/bucketlist/ThingsToDoActivity.java
@@ -3,6 +3,7 @@ package com.example.bucketlist;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class ThingsToDoActivity extends AppCompatActivity {
 
@@ -13,6 +14,46 @@ public class ThingsToDoActivity extends AppCompatActivity {
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+        setuplist();
+    }
+
+    private void setuplist() {
+        BucketListEntry[] thingsToDo = {
+                new BucketListEntry(
+                        "Skydiving",
+                        "Experience the thrill of jumping out of a plane!",
+                        R.drawable.skydive,
+                        3.2f
+                ),
+                new BucketListEntry(
+                        "Scuba Diving in the Great Barrier Reef",
+                        "Explore the underwater beauty of the world's largest coral reef.",
+                        R.drawable.road_trip,
+                        2.2f
+                ),
+                new BucketListEntry(
+                        "Visit the Eiffel Tower",
+                        "See the iconic landmark in Paris and enjoy breathtaking views.",
+                        R.drawable.kilimanjaro,
+                        4f
+                ),
+                new BucketListEntry(
+                        "Hike Machu Picchu",
+                        "Discover the ancient Incan city and its stunning surroundings.",
+                        R.drawable.scubadive,
+                        2.4f
+                ),
+                new BucketListEntry(
+                        "Northern Lights",
+                        "Witness the magical aurora borealis in Iceland or Norway.",
+                        R.drawable.iceland,
+                        1.9f
+                ),
+        };
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view_things_to_do);
+        BucketListEntryAdapter adapter = new BucketListEntryAdapter(thingsToDo);
+        recyclerView.setAdapter(adapter);
     }
 
     @Override
