86df122d4cc8c3297660eae40aa41fe00a0ae4c5
Feihuan Peng <feihuan.peng@spreetail.com>
2025-09-23 22:23:47 -0500

    holishit it worked

---
+++ app/src/main/java/com/example/bucketlist/PlacesToGoActivity.java
@@ -0,0 +1,23 @@
+package com.example.bucketlist;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlacesToGoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_places_to_go);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        getOnBackPressedDispatcher().onBackPressed();
+        return true;
+    }
+}
---
+++ app/src/main/java/com/example/bucketlist/ThingsToDoActivity.java
@@ -0,0 +1,23 @@
+package com.example.bucketlist;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ThingsToDoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_things_to_do);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        getOnBackPressedDispatcher().onBackPressed();
+        return true;
+    }
+}
